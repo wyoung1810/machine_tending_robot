72dd71e87b51565d78b8c357e977b5f2a2cd00a9
wyoung1810 <ukjoma@gmail.com>
2025-02-10 14:00:30 -0500

    Create cycle_start.ino

---
+++ cycle_start/cycle_start.ino
@@ -0,0 +1,29 @@
+#include "Keyboard.h"
+
+const int buttonPin = 4;         // input pin for pushbutton
+int previousButtonState = HIGH;  // for checking the state of a pushButton
+
+void setup() {
+  // make the pushButton pin an input:
+  pinMode(buttonPin, INPUT);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+}
+
+void loop() {
+  // read the pushbutton:
+  int buttonState = digitalRead(buttonPin);
+  // if the button state has changed,
+  if ((buttonState != previousButtonState)
+      // and it's currently pressed:
+      && (buttonState == HIGH)) {
+    // increment the button counter
+    counter++;
+    // type out a message
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+  }
+  // save the current button state for comparison next time:
+  previousButtonState = buttonState;
+}
