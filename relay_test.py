aab0fc71ad8184ae9d604a928e79cac792520de7
wyoung1810 <144073384+wyoung1810@users.noreply.github.com>
2025-03-26 15:32:29 -0400

    Added test code for USB relay

---
+++ relay_test.py
@@ -0,0 +1,16 @@
+from sainsmart_relay.relay import FTDIBitbangRelay
+import time
+
+# Initialize the relay controller with the device ID
+relay_controller = FTDIBitbangRelay(device_id='AB0NXT9F')
+
+# Turn relay 1 on
+for i in range(4):
+    relay_controller.set_relay(i+1, 'off')
+    time.sleep(1)
+    
+    
+
+# Retrieve the current relay state
+state = relay_controller.get_relay_state()
+print(f"Current Relay State: {state:08b}")
